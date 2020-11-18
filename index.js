b05fe2db736a8fd01384e58812997c37dd40010b
david espinal <despinal0425@gmail.com>
2020-11-17 22:55:27 -0500

    Having trouble with search bar not working.

---
+++ index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { search } from "./api"
+
+(() => {
+  const handleSearchButtonClick = () => {
+    const searchTerm = document.getElementById('search-pane-input').nodeValue;
+
+    search(searchTerm)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+    })
+  }
+
+  window.addEventListener('load', () => {
+    document.getElementById('search-pane-button')
+    .addEventListener('click', handleSearchButtonClick)
+  });
+})();
